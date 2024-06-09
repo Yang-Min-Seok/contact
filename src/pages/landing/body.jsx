0e17702fa874769b7c5656f90503a4b54df790ad
양민석 <kurooru98@gmail.com>
2024-06-09 20:58:22 +0900

    landing framing

--- src/pages/landing/body.jsx
+++ src/pages/landing/body.jsx
@@ -1,9 +1,36 @@
 import { BodyDiv } from "./style";
+import useInput from "../../hooks/useInput";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Body() {
+    
+    const navigate = useNavigate();
+    const [ court_num, set_court_num ] = useState('0');
+    const [ ppl_num, on_change_ppl_num, set_ppl_num ] = useInput('0');
+
+    const handle_onclick_courts_num = (e) => {
+        set_court_num(e.target.innerText);
+    };
+
+    const handle_onclick_start_btn = (e) => {
+        console.log(court_num, ppl_num);
+        navigate(`/courts`, {state : {court_num : court_num, ppl_num : ppl_num}});
+    };
+
     return (
-        
         <BodyDiv>
+            <h3><span>STEP1 </span>コート数を設定してください</h3>
+            <ul onClick={handle_onclick_courts_num}>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+            <h3><span>STEP2 </span>人数を設定してください</h3>
+            <p><input type="number" name="" id="" placeholder="例) 8" onChange={on_change_ppl_num}/> 名</p>
+            <h3><span>STEP3 </span>スタートボタンを押してください</h3>
+            <button type="button" onClick={handle_onclick_start_btn}>スタート</button>
         </BodyDiv>
     )
 }
--- src/pages/landing/style.jsx
+++ src/pages/landing/style.jsx
@@ -1,5 +1,91 @@
 import styled from "styled-components";
 
 export const BodyDiv = styled.div`
+    
+    font-family: sans-serif;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
+    h3:nth-child(1) {
+        span {
+            color: brown;
+        }
+        text-align: center;
+        margin: 0;
+        line-height: 3;
+        font-size: 130%;
+    }
+    
+    ul:nth-child(2) {
+        padding: 0;
+        display: flex;
+        width: 80%;
+        margin: 0 auto;
+        justify-content: space-between;
+        li {
+            width: calc(90% / 4);
+            list-style: none;
+            text-align: center;
+            font-size: 130%;
+            line-height: 2.5;
+            border: 2px solid brown;
+            box-sizing: border-box;
+            border-radius: 10%;
+            font-weight: 700;
+        }
+    }
+
+    h3:nth-child(3) {
+        span {
+            color: brown;
+        }
+        text-align: center;
+        margin: 0;
+        line-height: 3;
+        font-size: 130%;
+    }
+
+    p:nth-child(4) {
+        margin: 0;
+        text-align: center;
+        font-size: 130%;
+        font-weight: 500;
+        input {
+            border : 2px solid brown;
+            border-radius : 10px;
+            box-sizing: border-box;
+            line-height: 2;
+            padding: 0;
+            text-align: center;
+            width: 30%;
+            font-size: 100%;
+        }
+    }
+
+    h3:nth-child(5) {
+        span {
+            color: brown;
+        }
+        text-align: center;
+        margin: 0;
+        line-height: 3;
+        font-size: 130%;
+    }
+
+    button:nth-child(6) {
+        display: block;
+        margin: 0 auto;
+        background-color: #fff;
+        border : 2px solid brown;
+        border-radius: 10px;
+        width: 40%;
+        font-size: 130%;
+        line-height: 2;
+        padding: 0;
+        color: red;
+        font-weight: 600;
+    }
 
 `

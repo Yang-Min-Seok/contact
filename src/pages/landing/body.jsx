3a64b4fbd92a6ddba1759d2aa0b464a3b09fef26
양민석 <kurooru98@gmail.com>
2024-06-09 23:54:23 +0900

    landing implement

--- src/pages/landing/body.jsx
+++ src/pages/landing/body.jsx
@@ -10,22 +10,48 @@ function Body() {
     const [ ppl_num, on_change_ppl_num, set_ppl_num ] = useInput('0');
 
     const handle_onclick_courts_num = (e) => {
-        set_court_num(e.target.innerText);
+        try{
+
+            for (let i = 1; i <= 4; i++) {
+                const target = document.getElementById(`ppl_${i}`);
+                target.style.backgroundColor = `#fff`;
+                target.style.boxShadow = `0px 0px`;
+                target.style.color = `#000`;
+            }
+    
+            const target_num = e.target.innerText;
+            set_court_num(target_num);
+            const target = document.getElementById(`ppl_${target_num}`);
+            target.style.backgroundColor = `#eee`;
+            target.style.boxShadow = `2px 2px`;
+            target.style.color = `brown`;
+
+        } catch(err) {
+            console.log(err);
+        }
+        
     };
 
     const handle_onclick_start_btn = (e) => {
-        console.log(court_num, ppl_num);
-        navigate(`/courts`, {state : {court_num : court_num, ppl_num : ppl_num}});
+        if (court_num !== '0' && ppl_num !== '0') {
+            console.log(`court_num : ${court_num}, ppl_num : ${ppl_num}`);
+            navigate(`/courts`, {state : {court_num : court_num, ppl_num : ppl_num}});
+        } else if (court_num === '0'){
+            alert('check court_num');
+        } else {
+            alert('check ppl_num');
+        }
+        
     };
 
     return (
         <BodyDiv>
             <h3><span>STEP1 </span>コート数を設定してください</h3>
             <ul onClick={handle_onclick_courts_num}>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
+                <li id="ppl_1">1</li>
+                <li id="ppl_2">2</li>
+                <li id="ppl_3">3</li>
+                <li id="ppl_4">4</li>
             </ul>
             <h3><span>STEP2 </span>人数を設定してください</h3>
             <p><input type="number" name="" id="" placeholder="例) 8" onChange={on_change_ppl_num}/> 名</p>

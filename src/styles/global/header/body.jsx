36001b246fe77670748d6bb4ef4fd96a43777e75
양민석 <kurooru98@gmail.com>
2024-06-09 17:31:49 +0900

    header

--- src/styles/global/header/body.jsx
+++ src/styles/global/header/body.jsx
@@ -2,9 +2,13 @@ import { BodyDiv } from "./style";
 
 function Body() {
 
+    const handle_onclick_contact_logo = (e) => {
+        console.log(e);
+    }
+
     return (
         <BodyDiv>
-
+            <div id="contact_logo" onClick={handle_onclick_contact_logo}></div>
         </BodyDiv>
     );
 }
--- src/styles/global/header/style.jsx
+++ src/styles/global/header/style.jsx
@@ -1,5 +1,14 @@
 import styled from "styled-components";
+import contact_logo from "../../../assets/images/contact_logo.png";
 
 export const BodyDiv = styled.div`
-
+    div{
+        width: 50%;
+        height: 70px;
+        margin: 0 auto;
+        background-image: url(${contact_logo});
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: contain;
+    }
 `

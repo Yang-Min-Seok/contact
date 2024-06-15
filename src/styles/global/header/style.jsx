60f07ad6d284b809659fe66e79b3fdb9339850b5
양민석 <kurooru98@gmail.com>
2024-06-15 15:16:10 +0900

    header style update

--- src/styles/global/header/style.jsx
+++ src/styles/global/header/style.jsx
@@ -3,12 +3,12 @@ import contact_logo from "../../../assets/images/contact_logo.png";
 
 export const BodyDiv = styled.div`
     div{
-        width: 50%;
+        width: 60%;
         height: 70px;
         margin: 0 auto;
         background-image: url(${contact_logo});
         background-position: center;
         background-repeat: no-repeat;
-        background-size: contain;
+        background-size: cover;
     }
 `

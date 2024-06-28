6f1e299010d2755fc18787b6196117f5ac0ca2c7
양민석 <kurooru98@gmail.com>
2024-06-28 20:13:10 +0900

    footer update (add info of update)

--- src/styles/global/footer/body.jsx
+++ src/styles/global/footer/body.jsx
@@ -3,6 +3,7 @@ function Body() {
 
     return (
         <BodyDiv>
+            <p>Ver 1.0.0(Last update 2024.6.15)</p>
             <p>ⓒ 2024. Contact kurooru all rights reserved.</p>
         </BodyDiv>
     );

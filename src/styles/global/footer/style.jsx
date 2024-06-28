73ea938eb14bf45b23ebd86d01dbf2b7280c0efa
양민석 <kurooru98@gmail.com>
2024-06-28 20:42:55 +0900

    footer margin update

--- src/styles/global/footer/style.jsx
+++ src/styles/global/footer/style.jsx
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 
 export const BodyDiv = styled.div`
     p{  
-        margin: 3% 0;
+        margin: 2% 0;
         text-align: center;
         font-size: 60%;
         font-family: serif;

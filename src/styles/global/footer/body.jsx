eb046c722becf34ee134e1c8cfcbdcb0e790d56f
양민석 <kurooru98@gmail.com>
2024-06-29 13:34:07 +0900

    version update

--- src/styles/global/footer/body.jsx
+++ src/styles/global/footer/body.jsx
@@ -3,7 +3,7 @@ function Body() {
 
     return (
         <BodyDiv>
-            <p>Ver 1.0.1(Last update 2024.6.28)</p>
+            <p>Ver 1.0.2(Last update 2024.6.29)</p>
             <p>ⓒ 2024. Contact kurooru all rights reserved.</p>
         </BodyDiv>
     );
